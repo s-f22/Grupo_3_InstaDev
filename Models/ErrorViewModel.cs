acbf635669e3b97230f363ec340dea2800a621af
Samuel <samuel.figs@gmail.com>
2021-06-28 15:34:40 -0300

    inicio do repositorio

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Grupo_3_InstaDev.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
