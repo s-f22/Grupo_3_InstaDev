6838be7ca83906c5160f1311286c10f0594a6f03
Samuel <samuel.figs@gmail.com>
2021-07-02 11:42:27 -0300

    front end edicaoPerfil OK inicio backend 02-07

--- Models/InstaDev_G3_Base.cs
+++ Models/InstaDev_G3_Base.cs
@@ -5,10 +5,10 @@ namespace Grupo_3_InstaDev.Models
 {
     public class InstaDev_G3_Base
     {
-        public void CriarPastaEArquivo(string _caminho)
+        public void CriarPastaEArquivo(string _caminho)     // Metodo para criar arquivo e pasta conforme caminho fornecido pela string
         {
-            string pasta = _caminho.Split("/")[1];
-            string arquivo = _caminho.Split("/")[2];
+            string pasta = _caminho.Split("/")[0];
+            string arquivo = _caminho.Split("/")[1];
 
             if (!Directory.Exists(pasta))
             {
@@ -22,7 +22,7 @@ namespace Grupo_3_InstaDev.Models
 
         }
 
-        public List<string> LerTodasLinhasCSV(string _caminho)
+        public List<string> LerTodasLinhasCSV(string _caminho)      // Metodo para ler as linhas/conteudo do arquivo CSV, salvar este conteudo em uma lista de strings e retornar a LISTA DE STRINGS (não o arquivo csv) para acesso onde for chamada
         {
             List<string> linhas = new List<string>();
 
@@ -38,7 +38,7 @@ namespace Grupo_3_InstaDev.Models
             return linhas;
         }
 
-        public void ReescreverCSV(string _caminho, List<string> linhas)
+        public void ReescreverCSV(string _caminho, List<string> linhas)     // Metodo que recebe uma lista de strings, faz a leitura linha-a-linha da lista e converte os dados em um arquivo csv, conforme caminho especificado que tambem deve ser fornecido como parametro; funciona em conjunto com o metodo LerTodasLinhasCSV, acima
         {
             using (StreamWriter output = new StreamWriter(_caminho))
             {
