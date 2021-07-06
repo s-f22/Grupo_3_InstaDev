8ee1884218aca4a04246e6f409cde4e682d307bf
Samuel <samuel.figs@gmail.com>
2021-07-06 14:44:57 -0300

    inicio da continuacao backend 06-07

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+const logarBut = document.querySelector('.logarBut')
+const cadasBut = document.querySelector('.cadasBut')
+const formbx = document.querySelector('.formbx')
+
+cadasBut.onclick = function () {
+    formbx.classList.add('active')
+    body.classList.add('active')
+}
+logarBut.onclick = function () {
+    formbx.classList.remove('active')
+}
